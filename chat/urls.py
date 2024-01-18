cb3009ed7fb09470136bde7402c3857c9c858193
FelipeGomesArnus <felipe@arnusai.onmicrosoft.com>
2024-01-18 15:38:12 -0300

    Criando profile do usuario

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import edit_profile
 
 app_name= 'chat'
 
 urlpatterns = [
     path('home/', views.home, name = 'home'),
+    path('user/', views.user, name = 'user'),
+    path('user/edit/', edit_profile, name='edit_profile'),
 ]
